2a1c5275fc3fa410d54b080edbc04e6c9bee616c
Sing Painter <mail@erlan.xyz>
2018-05-28 11:30:14 +0300

    #6 partial implementation

--- api/golos-js/test/auto.js
+++ api/golos-js/test/auto.js
@@ -1,33 +1,55 @@
 const assert = require('assert');
-//const Promise = require('bluebird');
+const Promise = require('bluebird');
+const golos = require('golos-js');
 const fs = require('fs');
 const methodsListJson = fs.readFileSync('swagger.json');
-const methodsList = JSON.parse(methodsListJson);
-const golos = require('golos-js');
+let methodsList = JSON.parse(methodsListJson);
+methodsList = methodsList.paths;
+delete methodsList['/swagger'];
 //golos.config.set('websocket', 'wss://ws.testnet.golos.io');
 //golos.config.set('address_prefix', 'GLS');
 //golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
 
+let currentMethodIndex = 0;
+let methodsListKeys = Object.keys(methodsList);
 
-describe('start tests', () => {
-
-	/*beforeEach(() => {
-		return Promise.delay(100);
-	});*/
-
-	/* for (methodName in methodsList.paths) {
-		console.log(methodsList.paths[methodName]);
-	} */
-	// it runned
-	it('get_accounts', async ()=> {
-		let accounts = ['epexa'];
-		return await golos.api.getDynamicGlobalProperties(function(err, result) {
-		//return await golos.api.getAccounts(accounts, function(err, result) {
-			//if ( ! err) console.log(result.head_block_number, result.time, result.current_witness);
-			//else console.error(err);
-			console.log(err, result);
-		});
+let camelCase = function(str) {
+	return str.replace(/_([a-z])/g, function(_m, l) {
+		return l.toUpperCase();
 	});
+}
+
+//for (methodName in methodsList) {
+
+let runTestMethod = function() {
+	let methodName = methodsListKeys[currentMethodIndex];
+	if (methodName) {
+		currentMethodIndex++;
+		let method = methodsList[methodName];
+		methodName = methodName.substr(1);
+		methodName = camelCase(methodName);
+		
+		// run test method
+		describe('start tests', () => {
+			
+			beforeEach(() => {
+				return Promise.delay(1000);
+			});
+			
+			it(methodName, async ()=> {
+				console.log('method name: ', methodName);
+				return await eval(`golos.api.${methodName}(function(err, result) {
+					console.error(err);
+					if (currentMethodIndex < methodsListKeys.length) runTestMethod();
+				});`);
+			});
+		});
+	}
+}
+
+runTestMethod();
+
+//describe('start tests', () => {
 
 	/*afterAll(() => {
 		process.exit(-1);
@@ -37,4 +59,4 @@ describe('start tests', () => {
 		console.log('title: ', this.currentTest.title, 'duration: ', this.currentTest.duration + 'ms', 'state: ', this.currentTest.state);
 	});*/
 
-});
+//});
